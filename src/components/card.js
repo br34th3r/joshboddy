6bbf01ec35ee72a28208a889a9a257b1c2be9454
br34th3r <joshuaboddy1@gmail.com>
2019-12-13 04:47:21 +0000

    Fixed post list spacing

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ function card(props) {
   const { cardTitle, cardSubtitle, link } = props;
   return (
     <div className="col-lg-4">
-      <a href={link} className="card mx-3 my-5">
+      <a href={link} className="card my-3">
         <div className="card-body">
           <h2 className="card-title">{cardTitle}</h2>
           <p className="card-text">{cardSubtitle}</p>
