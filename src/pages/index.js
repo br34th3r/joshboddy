024de7fe9251e3cfd63a2b90cc77dff89a1b3e1c
br34th3r <joshuaboddy1@gmail.com>
2019-12-22 13:26:30 +0100

    Added meta data to the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,10 @@ function index() {
       <Info />
       <Helmet>
         <meta charSet="utf-8" />
+        <meta name="description" content="Josh Boddy | Computer Science and all things tech" />
+        <meta name="keywords" content="Programming, Josh, Boddy, Joshua, City, University, Computing, Computer, Science, Engineering, Tech, Technology, Music, Acting, Theatre, Film, Guitar, Vocal, Song" />
+        <meta name="author" content="Josh Boddy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Home | Josh</title>
       </Helmet>
     </div>
